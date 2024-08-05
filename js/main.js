d67821b53a3a0111f02bdfa25beccbfe57bb4c3b
Nicholas Kennedy <nmk10062@gmail.com>
2024-08-05 13:24:01 -0400

    Add captcha to contact form and handle form submission without redirect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
+const contactForm = document.getElementById("contactForm");
+const formLoading = document.getElementById("formLoading");
+const formResult = document.getElementById("formResult");
+
 window.addEventListener("load", updateScrollItems);
 window.addEventListener("scroll", updateScrollItems);
+
+function submitContact() {
+  const formData = new FormData(contactForm);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  contactForm.style.visibility = "hidden";
+  formLoading.classList.remove("hidden");
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        formLoading.classList.add("hidden");
+        formResult.classList.remove("hidden");
+      } else {
+        formResult.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      formResult.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      contactForm.reset();
+    });
+}
 
 function updateScrollItems() {
   const scrollUpdateItems = [
